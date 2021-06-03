1ea626f25886e435c23930ec6ddf202d63924b86
Mateus-Leandro <mateusleandro2205@gmail.com>
2021-06-03 10:38:11 -0300

    Movimento especial castling

--- src/xadrex/PartidaXadrex.java
+++ src/xadrex/PartidaXadrex.java
@@ -97,6 +97,24 @@ public class PartidaXadrex {
 			pecasCapturadas.add(peca_capturada);
 		}
 
+		// #Specialmove castling kingside rook
+		if (p instanceof Rei && destino.getColuna() == origem.getColuna() + 2) {
+			Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() + 3);
+			Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() + 1);
+			PecaXadrex torre = (PecaXadrex) tabuleiro.removerPeca(origemT);
+			tabuleiro.ColocarPeca(torre, destinoT);
+			torre.incrementaMovimento();
+		}
+
+		// #Specialmove castling queenside rook
+		if (p instanceof Rei && destino.getColuna() == origem.getColuna() - 2) {
+			Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() - 4);
+			Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() - 1);
+			PecaXadrex torre = (PecaXadrex) tabuleiro.removerPeca(origemT);
+			tabuleiro.ColocarPeca(torre, destinoT);
+			torre.incrementaMovimento();
+		}
+
 		return peca_capturada;
 	}
 
@@ -109,6 +127,24 @@ public class PartidaXadrex {
 			tabuleiro.ColocarPeca(pecaCapturada, destino);
 			pecasCapturadas.remove(pecaCapturada);
 			pecasDoTabuleiro.add(pecaCapturada);
+		}
+
+		// #Specialmove castling kingside rook
+		if (p instanceof Rei && destino.getColuna() == origem.getColuna() + 2) {
+			Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() + 3);
+			Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() + 1);
+			PecaXadrex torre = (PecaXadrex) tabuleiro.removerPeca(destinoT);
+			tabuleiro.ColocarPeca(torre, origemT);
+			torre.decrementaMovimento();
+		}
+
+		// #Specialmove castling queenside rook
+		if (p instanceof Rei && destino.getColuna() == origem.getColuna() - 2) {
+			Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() - 4);
+			Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() - 1);
+			PecaXadrex torre = (PecaXadrex) tabuleiro.removerPeca(destinoT);
+			tabuleiro.ColocarPeca(torre, origemT);
+			torre.decrementaMovimento();
 		}
 	}
 
@@ -199,7 +235,7 @@ public class PartidaXadrex {
 		colocarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('d', 1, new Rainha(tabuleiro, Cor.BRANCO));
-		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO, this));
 		colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
@@ -216,7 +252,7 @@ public class PartidaXadrex {
 		colocarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('d', 8, new Rainha(tabuleiro, Cor.PRETO));
-		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO, this));
 		colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETO));
--- src/xadrex/pecas/Rei.java
+++ src/xadrex/pecas/Rei.java
@@ -3,23 +3,31 @@ package xadrex.pecas;
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrex.Cor;
+import xadrex.PartidaXadrex;
 import xadrex.PecaXadrex;
 
 public class Rei extends PecaXadrex {
-	
 
-	public Rei(Tabuleiro tabuleiro, Cor cor) {
+	private PartidaXadrex partida;
+
+	public Rei(Tabuleiro tabuleiro, Cor cor, PartidaXadrex partida) {
 		super(tabuleiro, cor);
+		this.partida = partida;
 	}
 
 	@Override
 	public String toString() {
 		return "R";
 	}
-	
+
 	private boolean podeMover(Posicao posicao) {
 		PecaXadrex p = (PecaXadrex) getTabuleiro().peca(posicao);
 		return p == null || p.getCor() != getCor();
+	}
+
+	private boolean testeReiCastling(Posicao posicao) {
+		PecaXadrex p = (PecaXadrex) getTabuleiro().peca(posicao);
+		return p != null && p instanceof Torre && p.getCor() == getCor() && p.getContadorDeMovimentos() == 0;
 	}
 
 	@Override
@@ -74,6 +82,31 @@ public class Rei extends PecaXadrex {
 		p.setPosicao(posicao.getLinha() + 1, posicao.getColuna() + 1);
 		if (getTabuleiro().existePosicao(p) && podeMover(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// #Special move Castling
+		if (getContadorDeMovimentos() == 0 && !partida.getcheck()) {
+			// # Specialmove castling kingside rook
+			Posicao posT1 = new Posicao(posicao.getLinha(), posicao.getColuna() + 3);
+			if (testeReiCastling(posT1)) {
+				Posicao p1 = new Posicao(posicao.getLinha(), posicao.getColuna() + 1);
+				Posicao p2 = new Posicao(posicao.getLinha(), posicao.getColuna() + 2);
+				if (getTabuleiro().peca(p1) == null && getTabuleiro().peca(p2) == null) {
+					mat[posicao.getLinha()][posicao.getColuna() + 2] = true;
+				}
+			}
+
+			// # Specialmove castling queenside rook
+			Posicao posT2 = new Posicao(posicao.getLinha(), posicao.getColuna() - 4);
+			if (testeReiCastling(posT2)) {
+				Posicao p1 = new Posicao(posicao.getLinha(), posicao.getColuna() - 1);
+				Posicao p2 = new Posicao(posicao.getLinha(), posicao.getColuna() - 2);
+				Posicao p3 = new Posicao(posicao.getLinha(), posicao.getColuna() - 3);
+				if (getTabuleiro().peca(p1) == null && getTabuleiro().peca(p2) == null
+						&& getTabuleiro().peca(p3) == null) {
+					mat[posicao.getLinha()][posicao.getColuna() - 2] = true;
+				}
+			}
 		}
 
 		return mat;
